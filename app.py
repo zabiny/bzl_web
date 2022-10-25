0b2fa2d8e88f5b44b50aa41b4717f9336490edd5
Jakub Hruska <jakub.hruska@innovatrics.com>
2022-10-25 20:50:43 +0200

    Add basic pages structure

--- app.py
+++ app.py
@@ -10,6 +10,24 @@ def home():
     return render_template("home.html")
 
 
+# Calendar
+@app.route("/<string:season>/calendar")
+def calendar(season: str):
+    return render_template("calendar.html", season=season)
+
+
+# Results
+@app.route("/<string:season>/results")
+def results(season: str):
+    return render_template("results.html", season=season)
+
+
+# Race
+@app.route("/<string:season>/races/<int:race_id>/")
+def race(season: str, race_id: int):
+    return render_template("race.html")
+
+
 def main():
     app.run(port=5000, debug=True)
 
