594c2d16f53e7e46bcb2705af132ed189e3c3fc1
Drakos <matyas.oujezdsky@gmail.com>
2022-11-18 17:45:30 +0100

    Added silhouetts of runners, only works on 16:9

--- static/script/script.js
+++ static/script/script.js
@@ -3,3 +3,8 @@ const year = date.getFullYear();
 
 document.getElementById("zak").innerHTML = year - 13;
 document.getElementById("veteran").innerHTML = year - 45;
+
+if (Math.random() < 0.005)
+{
+    document.getElementById("easter-egg").src = "/static/runner69.png";
+}
