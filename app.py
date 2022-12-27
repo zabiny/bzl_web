0df2644aa3751bfe5ed02a62a9e1d0b93730d967
Jakub Hruska <jakub.hruska@innovatrics.com>
2022-12-27 17:28:04 +0100

    Implement results page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import atexit, math
+import atexit
 import locale
 from datetime import date
 
+import pandas as pd
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, redirect, render_template, url_for
 
@@ -38,7 +39,52 @@ def calendar(season: str):
 # Results
 @app.route("/<string:season>/results")
 def results(season: str):
-    return render_template("results.html", season=season)
+    # Load results per category
+    category_dfs = []
+    for category in ["H", "D", "ZV", "HDD"]:
+        df = pd.read_csv(f"data/{season}/results/overall_{category}.csv", index_col=0)
+        df["category"] = category
+        for place_col in df.filter(regex=r".*-Place"):
+            if df[place_col].dtype == float:
+                df[place_col] = df[place_col].apply(lambda x: f"{x:.0f}.")
+        category_dfs.append(df)
+    df = pd.concat(category_dfs)
+
+    # Process DataFrame (rename and drop columns etc.)
+    events = em.get_all_events(season)
+    oris_id_to_name_mapping = {
+        ev.oris_id: ev.name for ev in events.values() if ev.oris_id
+    }
+
+    oris_ids_in_results = set(
+        [int(x.split("-")[0]) for x in df.columns if x[0].isdigit()]
+    )
+
+    cols_to_drop = []
+    for oris_id in oris_ids_in_results:
+        df[oris_id_to_name_mapping[oris_id]] = (
+            df[f"{oris_id}-Points"].astype(str)
+            + " ("
+            + df[f"{oris_id}-Place"].astype(str)
+            + ")"
+        )
+        cols_to_drop.extend([f"{oris_id}-Points", f"{oris_id}-Place"])
+
+    best_n_col = df.filter(regex=r"Best.*").columns[0]
+    n = best_n_col.split("-")[0][4:]
+    df = df.rename(
+        columns={
+            best_n_col: f"Součet ({n} z {len(oris_ids_in_results)})",
+            "Name": "Jméno",
+        }
+    ).drop(columns=cols_to_drop)
+
+    # Split DataFrame per category
+    results = {}
+    for category, group in df.groupby("category"):
+        results[category] = group.drop(columns=["category"])
+
+    return render_template("results.html", season=season, results=results)
 
 
 # Event
@@ -85,6 +131,12 @@ def _filter_date_from_datetime(input_datetime: str):
     d = date.fromisoformat(string_date)
     czech_date = d.strftime("%d. %m. %Y")
     return czech_date
+
+
+@app.template_filter("full_season")
+def _filter_full_season(season_short: str):
+    year_from, year_to = season_short.split("-")
+    return f"20{year_from} - 20{year_to}"
 
 
 def main():
